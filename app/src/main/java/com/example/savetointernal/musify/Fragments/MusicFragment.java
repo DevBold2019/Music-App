53bd9a671c4ece632db3a0d1114ddde6cf5bb06a
Developer Bold <53851146+DevBold2019@users.noreply.github.com>
2021-02-16 21:26:43 +0300

    next song play

--- app/src/main/java/com/example/savetointernal/musify/Fragments/MusicFragment.java
+++ app/src/main/java/com/example/savetointernal/musify/Fragments/MusicFragment.java
@@ -1,7 +1,9 @@
 package com.example.savetointernal.musify.Fragments;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.media.MediaPlayer;
@@ -34,29 +36,31 @@ import com.example.savetointernal.musify.R;
 import com.example.savetointernal.musify.Fragments.Adapters.MyAdapter;
 import com.example.savetointernal.musify.Fragments.Models.SongInfoModel;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Objects;
+import java.util.Random;
 
 
 public class MusicFragment extends Fragment {
 
     ArrayList<SongInfoModel> songs;
-    RecyclerView recyclerView; //recy
-    private Handler myHandler = new Handler();;
-    SeekBar seekBar; //seekbar
-    MediaPlayer mediaPlayer;
+    RecyclerView recyclerView;
+    SeekBar seekBar;
+    MediaPlayer mediaPlay;
     MyAdapter songAdapter;
     TextView SName;
     TextView AName;
     ImageButton imb,imb1,next;
-    int length,len;
-    int playlist;
+    int length;
     ConstraintLayout constraintLayout;
-    int maximum;
     CardView cardView;
-    int songIndex=0;
-    Runnable runnable;
     boolean isItPaused;
+    ArrayList<SongInfoModel> songsList;
+    int posit,times=1;
+    boolean isLoopActive=false;
+    Random random=new Random();
+    Thread thread = new showProgress();
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -82,7 +86,7 @@ public class MusicFragment extends Fragment {
         next=view.findViewById(R.id.nextMusic);
 
         Intent intent = Objects.requireNonNull(getActivity()).getIntent();
-        final ArrayList<SongInfoModel> songsList = intent.getParcelableArrayListExtra("songs");
+        songsList = intent.getParcelableArrayListExtra("songs");
 
         Toast.makeText(getContext(),"SIZE IS :\t"+songsList.size(),Toast.LENGTH_SHORT).show();
 
@@ -90,6 +94,8 @@ public class MusicFragment extends Fragment {
         songs=new ArrayList<>();
         songAdapter = new MyAdapter(getContext(),songsList);
         recyclerView.setAdapter(songAdapter);
+
+        Objects.requireNonNull(recyclerView.getLayoutManager()).scrollToPosition(songAdapter.getItemCount()-1);
 
 
         songAdapter.setOnItemClickListener(new MyAdapter.OnItemClickListener() {
@@ -99,107 +105,39 @@ public class MusicFragment extends Fragment {
                 constraintLayout.setVisibility(View.VISIBLE);
                 imb.setVisibility(View.GONE);
                 imb1.setVisibility(View.GONE);
+                posit=position;
 
-                if(mediaPlayer==null){
 
-                    Toast.makeText(getContext(),"Select a song",Toast.LENGTH_SHORT).show();
-
+                if(mediaPlay==null){
+                    seekBar.setProgress(0);
                 }else{
 
-                    mediaPlayer.stop();
-                    mediaPlayer=null;
+                    mediaPlay.stop();
+                    mediaPlay=null;
+
 
                 }
-
-
                         try {
-                            mediaPlayer = new MediaPlayer();
-                            mediaPlayer.setDataSource(obj.getSongUrl());
+                            mediaPlay = new MediaPlayer();
+                            mediaPlay.setDataSource(obj.getSongUrl());
 
                             AName.setText(obj.getArtistname());
                             SName.setText(obj.getSongname());
 
-                            mediaPlayer.prepareAsync();
-                            mediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+                            mediaPlay.prepareAsync();
+
+                            mediaPlay.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
 
                                 @Override
                                 public void onPrepared(final MediaPlayer mediaPlayer) {
 
                                     mediaPlayer.start();
-
                                     imb1.setVisibility(View.VISIBLE);
-                                    seekBar.setMax(mediaPlayer.getDuration());
 
+                                    Log.d("DURATION is:",""+mediaPlay.getDuration()/1000/60);
 
-                                    imb1.setOnClickListener(new View.OnClickListener() {
-                                        @Override
-                                        public void onClick(View v) {
-
-                                            imb1.setVisibility(View.GONE);
-                                            imb.setVisibility(View.VISIBLE);
-                                            isItPaused=true;
-
-                                            mediaPlayer.pause();
-                                            mediaPlayer.getCurrentPosition();
-
-
-
-
-                                        }
-                                    });
-
-
-                                    imb.setOnClickListener(new View.OnClickListener() {
-                                        @Override
-                                        public void onClick(View v) {
-
-                                            imb.setVisibility(View.GONE);
-                                            imb1.setVisibility(View.VISIBLE);
-                                            isItPaused=false;
-
-                                            length = mediaPlayer.getCurrentPosition();
-                                            mediaPlayer.seekTo(length);
-                                            mediaPlayer.start();
-
-
-                                        }
-                                    });
-
-                                    seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-                                        @Override
-                                        public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
-
-
-                                        }
-
-                                        @Override
-                                        public void onStartTrackingTouch(SeekBar seekBar) {
-
-                                        }
-
-                                        @Override
-                                        public void onStopTrackingTouch(SeekBar seekBar) {
-
-                                            if (!isItPaused){
-                                                mediaPlayer.seekTo(seekBar.getProgress());
-                                                mediaPlayer.start();
-                                                return;
-                                            }
-                                            seekBar.setProgress(seekBar.getProgress());
-
-
-                                        }
-                                    });
-
-                                    next.setOnClickListener(new View.OnClickListener() {
-                                        @Override
-                                        public void onClick(View v) {
-
-                                            Toast.makeText(getContext(), "loading ..", Toast.LENGTH_SHORT).show();
-                                            mediaPlayer.stop();
-
-                                        }
-                                    });
+                                    seekBar.setMax(mediaPlay.getDuration());
+                                    seekBar.setProgress(mediaPlayer.getCurrentPosition());
 
                                 }
                             });
@@ -213,11 +151,127 @@ public class MusicFragment extends Fragment {
         });
 
 
+        imb1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                imb1.setVisibility(View.GONE);
+                imb.setVisibility(View.VISIBLE);
+                isItPaused=true;
+
+                mediaPlay.pause();
 
 
 
-        Thread thread = new showProgress();
+
+            }
+        });
+
+
+        imb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                imb.setVisibility(View.GONE);
+                imb1.setVisibility(View.VISIBLE);
+                isItPaused=false;
+
+                length=seekBar.getProgress();
+
+                mediaPlay.seekTo(length);
+                mediaPlay.start();
+
+
+
+            }
+        });
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+
+                seekBar.setProgress(i);
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+                if (!isItPaused){
+                    mediaPlay.seekTo(seekBar.getProgress());
+                    mediaPlay.start();
+                    return;
+                }
+                int posy=seekBar.getProgress();
+                seekBar.setProgress(posy);
+
+
+
+            }
+        });
+
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                mediaPlay.stop();
+                mediaPlay.reset();
+                mediaPlay=null;
+
+                int nextPos= posit + times++;
+                int size=songsList.size();
+
+
+                if (isLoopActive){
+
+                   int randInt=random.nextInt(50);
+                    int nextPosLoop= posit + randInt;
+
+                    if (nextPosLoop > songsList.size()){
+
+                        nextPosLoop=randInt;
+
+                    }
+
+                    Objects.requireNonNull(recyclerView.getLayoutManager()).scrollToPosition(nextPosLoop);
+                    //playNextMusic(nextPos);
+                    return;
+                }
+
+                if (nextPos >= size){
+
+                    posit=0;
+                    nextPos=posit;
+                    Log.d("TIMES IS :", ""+nextPos);
+                    Objects.requireNonNull(recyclerView.getLayoutManager()).scrollToPosition(nextPos);
+                    playNextMusic(nextPos);
+                    seekBar.setProgress(0);
+                    times=1;
+
+
+                }else{
+
+                    Log.d("SIZE IS :", ""+nextPos);
+                    playNextMusic(nextPos);
+
+                }
+
+
+
+
+            }
+        });
+
+
+
         thread.start();
+
+
 
         return view;
 
@@ -237,27 +291,64 @@ public class MusicFragment extends Fragment {
                     e.printStackTrace();
                 }
 
-                if (mediaPlayer !=null){
+                if (mediaPlay !=null){
 
 
                     seekBar.post(new Runnable() {
                         @Override
                         public void run() {
 
-                            seekBar.setProgress(mediaPlayer.getCurrentPosition());
-                            if (isItPaused){
-                                seekBar.getProgress();
+                            if (!isItPaused){
+                                seekBar.setProgress(mediaPlay.getCurrentPosition());
+                                return;
                             }
+                            int pos=seekBar.getProgress();
+                            seekBar.setProgress(pos);
+
 
 
                         }
                     });
+                }else{
+                    seekBar.setProgress(0);
                 }
 
 
             }
             
         }
+    }
+
+    public void playNextMusic( int position){
+
+        imb.setVisibility(View.GONE);
+        imb1.setVisibility(View.GONE);
+
+        mediaPlay=new MediaPlayer();
+
+        try {
+
+            AName.setText(songsList.get(position).getArtistname());
+            SName.setText(songsList.get(position).getSongname());
+
+            mediaPlay.setDataSource(songsList.get(position).getSongUrl());
+            mediaPlay.prepareAsync();
+            mediaPlay.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+                @Override
+                public void onPrepared(MediaPlayer mediaPlayer) {
+
+                    mediaPlayer.start();
+                    imb1.setVisibility(View.VISIBLE);
+                }
+            });
+
+
+
+        }catch (Exception e){
+
+        }
+
+
     }
 
 
